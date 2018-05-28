163915d966a5a221756b1437fb7bc2c4cc4c9ab6
wizard <wizard@shield.money>
2018-05-28 18:13:19 +0200

    Added hint.

--- src/digital/fingerprints/Mumbling.java
+++ src/digital/fingerprints/Mumbling.java
@@ -1,6 +1,8 @@
 package digital.fingerprints;
 
 /* Celem jest zaimplementować metodę mumble w taki sposób, aby przejść testy w 100%.
+
+Hint: Dopuszczalny input to String parsowalny wyrażeniem regualarnym: [a-zA-Z]*
  */
 
 public class Mumbling {
